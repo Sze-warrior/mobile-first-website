b7a380ec3833d368b1c0e601fd517a46569ffc07
Michael Szeto <szetomj@gmail.com>
2016-05-21 18:57:54 +1200

    fixed nav to appear on desktop even after resized and toggled

--- scripts/header.js
+++ scripts/header.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 	var headerTitle = $('#title');
 
 	$('#navbar-toggle').on('click', function(){
-
 		$('.nav-list').slideToggle();
-
 	});
 
+    $(window).on('resize', function(){
+        var nav = $('nav ul');
+        $(nav).css('display', '');
+    });	
 });
